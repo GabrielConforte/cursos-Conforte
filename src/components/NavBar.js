cc6fc36791e6a5792d8474cd7be794f91c9b0151
GabrielConforte <82458582+GabrielConforte@users.noreply.github.com>
2021-06-17 10:54:26 -0300

    NavBar Actualizado

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,20 +8,29 @@ function NavBar(){
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand">Navegacion</a>
+    <a className="navbar-brand">"Logo"</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapsed" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" >Inicio</a>
+          <a className="nav-link">Programacion</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link">Listas</a>
+          <a className="nav-link">Diseño</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link">acerca de</a>
+          <a className="nav-link">Arte Digital</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">Productividad</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">Material Didactico</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">Acerca de...</a>
         </li>
         <li>
         <CartWidget/>

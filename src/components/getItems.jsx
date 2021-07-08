809bb47cccac978d5dd8b623b53280fe45d8c0d4
GabrielConforte <82458582+GabrielConforte@users.noreply.github.com>
2021-07-07 21:22:02 -0300

    Update getItems.jsx

--- src/components/getItems.jsx
+++ src/components/getItems.jsx
@@ -3,11 +3,11 @@ export async function getData(a) {
    console.log(a)
     const response = await fetch('https://60e3b3496c365a0017839363.mockapi.io/cursos/conforte/Cursos')
     let data = await response.json()
-    if(a.item == 'home'){
+    if(a.item === 'home'){
         console.log(data)
         return data;
     } else{
-        data = data.filter((i) => i.categoria == a.item)
+        data = data.filter((i) => i.categoria === a.item)
         return data;
     }
 

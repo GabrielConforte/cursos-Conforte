85ef8d03a0a4c3bfc299cc351cd6eb26708406cd
GabrielConforte <82458582+GabrielConforte@users.noreply.github.com>
2021-08-15 23:19:08 -0300

    Marca Sapiens

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import React from "react";
 function Home() {
   return (
       <div className="container text-center">
-<img className="mt-2 rounded" src="https://github.com/GabrielConforte/cursos-Conforte/blob/master/doc%20files/sapiens_logo.gif" alt="Lamp"></img>
+<img className="mt-2 rounded" src="https://github.com/GabrielConforte/cursos-Conforte/blob/master/doc%20files/Marca_sapiens_logo.png?raw=true" alt="Lamp"></img>
         <div className="m-3">
         <div><h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,.</h3></div>

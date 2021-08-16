3e583e9e4af5aef251758252bdd9180911888aa2
GabrielConforte <82458582+GabrielConforte@users.noreply.github.com>
2021-08-15 23:48:44 -0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import React from "react";
 function Home() {
   return (
       <div className="container text-center">
-<img className="mt-2 rounded img-fluid h-50" src="https://firebasestorage.googleapis.com/v0/b/cursos-conforte.appspot.com/o/imagenes%2Fsapiens_logo.png?alt=media&token=f16a624b-e966-48c7-9952-cf6a27e93016" alt="Sapiens Logo"></img>
+<img className="mt-2 rounded img-fluid" src="https://firebasestorage.googleapis.com/v0/b/cursos-conforte.appspot.com/o/imagenes%2Fsapiens_logo.png?alt=media&token=f16a624b-e966-48c7-9952-cf6a27e93016" alt="Sapiens Logo"></img>
         <div className="m-3">
         <div><h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,.</h3></div>
